90b6555500cc6060ac0ee9bee54ad7ad4d3f6de9
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-07-13 09:12:24 +0530

    Use mongoose models functions and validators

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,7 @@
 //require mongooose package
 const mongoose=require('mongoose')
+//require validator
+const validator=require('validator')
 
 //create mongoose connection
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
@@ -8,52 +10,73 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useCreateIndex:true
 })
 
-// //define basic model for user
+//define basic model for user
 
-// const User=mongoose.model('User',{
-//     name:{
-//         type:String         //define what should be the type of name in db
-//     },
-//     age:{
-//         type: Number        //define type of age
-//     }
-// })
-
-// //create instances of model
-
-// const me=new User({
-//     name:'Bhawana Gaur',
-//     age:20
-// })
-
-// //to save instance to the database
-
-// me.save().then((me)=>{
-//     console.log(me);
-// }).catch((error)=>{
-//     console.log('Error ',error);
-// })
-
-// Model for the tasks
-
-const Tasks=mongoose.model('Tasks',{
-    description:{
-        type:String
+const User=mongoose.model('User',{
+    name:{
+        type:String,        //define what should be the type of name in db
+        required:true,       //this make it important to give this property else got an error
+        trim:true           //trim the string and remove extra spaces
     },
-    completed:{
-        type:Boolean
+    age:{
+        type: Number,        //define type of age
+        default:0,              //give default value 
+        validate(value){
+            if(value<0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,         //change into lowercase
+        // validator for email is valid or not
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     }
 })
 
-//create instance of task
-const task=new Tasks({
-    description:'Do the homework',
-    completed:false
+//create instances of model
+
+const me=new User({
+    name:'   Bhawana Gaur    ',
+    email:'Bhawanagaur143@gmail.com',
+    age:20
 })
 
-//save the task
-task.save().then((task)=>{
-    console.log(task);
+//to save instance to the database
+
+me.save().then((me)=>{
+    console.log(me);
 }).catch((error)=>{
-    console.log(error)
+    console.log('Error ',error);
 })
+
+// Model for the tasks
+
+// const Tasks=mongoose.model('Tasks',{
+//     description:{
+//         type:String
+//     },
+//     completed:{
+//         type:Boolean
+//     }
+// })
+
+// //create instance of task
+// const task=new Tasks({
+//     description:'Do the homework',
+//     completed:false
+// })
+
+// //save the task
+// task.save().then((task)=>{
+//     console.log(task);
+// }).catch((error)=>{
+//     console.log(error)
+// })
