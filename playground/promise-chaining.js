45b77c7fe0bc68eace5a1210a03f0bb230afcf3b
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-07-22 10:57:59 +0530

    Async/Await example

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,13 +2,27 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
-//find and update property using promise chaining
+// find and update property using promise chaining
 
 User.findByIdAndUpdate('60f26d4190b7170208ecff92', { age: 20 }).then((user) => {
     console.log(user);
     return User.countDocuments({ age: 20 })
-}).then((result)=>{
+}).then((result) => {
     console.log(result);
+}).catch((e) => {
+    console.log(e);
+})
+
+// ASYNC AND AWAIT EXAMPLE
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('60f26d4190b7170208ecff92',2).then((count)=>{
+    console.log(count);
 }).catch((e)=>{
     console.log(e);
 })
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -11,3 +11,17 @@ Task.findByIdAndRemove('60f2714385128908fc61ef2c').then((task) => {
 }).catch((e) => {
     console.log(e);
 })
+
+// ASYNC AWAIT FUNCTION EXAMPLE
+
+const deleteAndCount = async (id, completed) => {
+    const task = await Task.findByIdAndRemove(id, { completed })
+    const count = await Task.countDocuments({ completed })
+    return count
+}
+
+deleteAndCount('60eadf97b34fee2ce07e54ad',false).then((count)=>{
+    console.log(count);
+}).catch((e)=>{
+    console.log(e);
+})
